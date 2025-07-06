4912107a27c4e6e5689c523df64ac1d22bf55edb
HRISHIK DEY <HRISHIKDEY75@GMAIL.COM>
2025-07-06 16:28:51 +0530

    a

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://signature-app-h7lz.onrender.com/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api',
 });
 
 export const register = (data) => API.post('/auth/register', data);
